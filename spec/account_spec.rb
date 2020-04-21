a5477a73b2e3d3fabe5230e7735c0f51d3b42824
dariathompson <daria.kambalina@gmail.com>
2020-04-21 12:16:58 +0100

    customer can deposit money to their account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,7 +2,17 @@
 
 require 'account'
 describe Account do
-  it 'initializes new account with balance 0' do
-    expect(subject.balance).to eq 0
+  describe '#initialize' do
+    it 'initializes new account with balance 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#deposit' do
+    it 'adds money to account balance' do 
+      subject.deposit(300)
+      subject.deposit(100)
+      expect(subject.balance).to eq 400
+    end
   end
 end
