ae4929694370bd7677b5fcccf7d5c4efbf7e59d6
dariathompson <daria.kambalina@gmail.com>
2020-04-22 13:57:06 +0100

    print statement after deposit was made

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'account'
 class Statement
   attr_reader :account
   def initialize(account)
@@ -8,5 +8,8 @@ class Statement
 
   def show
     puts 'date || credit || debit || balance'
+    if @account.transactions.length > 0
+    puts "#{@account.transactions[0].date} || || #{'%.2f' % @account.transactions[0].amount} || #{'%.2f' % @account.balance}"
+    end
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,6 +4,8 @@ require 'statement'
 describe Statement do
   let(:account) { double :account }
   let(:statement) { Statement.new(account) }
+  let(:date) { Time.now.strftime('%d/%m/%Y') }
+  let(:deposit) { double :transaction, date: date, type: 'deposit', amount: 1000}
   describe '#initialize' do
     it 'initializes with account' do
       expect(statement.account).to eq account
@@ -11,8 +13,19 @@ describe Statement do
   end
 
   describe '#show' do
+    let(:account) { double :account, transactions: [], balance: 0 }
     it 'prints empty statement if no transactions have been made' do
-      expect { statement.show }.to output("date || credit || debit || balance\n").to_stdout
+      statement_text = "date || credit || debit || balance\n"
+      expect { statement.show }.to output(statement_text).to_stdout
     end
+
+    context 'deposit was made' do
+      let(:account) { double :account, transactions: [deposit], balance: 1000 }
+      it 'prints statement with details of transaction' do
+        statement_text = "date || credit || debit || balance\n#{date} || || 1000.00 || 1000.00\n"
+        expect { statement.show }.to output(statement_text).to_stdout
+      end
+    end
+
   end
 end
